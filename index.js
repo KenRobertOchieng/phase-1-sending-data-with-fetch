e4cac084349ea90fea6cac583378786a704fe689
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-03-23 16:45:49 +0300

    modified db.json

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name,email){
+    const myDetails={
+        name:name,
+        email:email
+    };
+    return fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify(myDetails)
+    })
+        .then(res=>res.json())
+        .then(data=>{
+            const body = document.querySelector('body');
+            const p = document.createElement('p');
+            p.textContent = data.id; // append the returned ID (e.g., 592)
+            body.appendChild(p);
+        })
+        .catch(error=>{
+            const body = document.querySelector('body');
+      const p = document.createElement('p');
+      p.textContent = error.message; // e.g., "Unauthorized Access"
+      body.appendChild(p);
+    })
+}
+submitData("Ken","@ken123")
